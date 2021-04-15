d254bf94ee0d276e3b0c9ba73e99df77f4ac14b1
Aleksandra <solveug@rambler.ru>
2021-04-15 16:19:30 +0300

    update barbershop

--- app1.rb
+++ app1.rb
@@ -4,14 +4,38 @@ get '/' do
   erb :index
 end
 
+get '/contacts' do
+  @title = 'Contacts'
+  @message = 'Phone number: 1122'
+
+  erb :message
+end
+
+get '/faq' do
+  @title = 'FAQ'
+  @message = 'Under construction'
+
+  erb :message
+end
+
+get '/something' do
+  @title = 'Something'
+  @message = 'Blabla'
+
+  erb :message
+end
+
 post '/' do
   @login = params[:login]
   @pasword = params[:pasword]
 
   if @login == 'admin' && @pasword == 'secret'
-      erb :welcome
+    erb :welcome
+  elsif @login == 'admin' && @pasword == 'admin'
+    @message = 'Nise try! Access denied'
+    erb :index
   else
-      'Access denied'
+    @message = 'Access denied'
+    erb :index
   end
-  erb :index
 end
