3f750472ad8fb74693a3d5a7a92753e19a8d9c28
Aleksandra <solveug@rambler.ru>
2021-04-20 14:57:34 +0300

    create db on app init

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+  @db = SQLite3::Database.new 'barbershop.db'
+  @db.execute 'CREATE TABLE IF NOT EXISTS
+  "Users"
+  (
+  "id"  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "username"  TEXT,
+  "phone" TEXT,
+  "darestamp" TEXT,
+  "barber"  TEXT,
+  "color" TEXT
+  )'
+end
 
 get '/' do
 	erb :index
